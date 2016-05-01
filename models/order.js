850058584541d2f30575e1c89b5c464ad8589b21
Pongsakorn Somsri <pongsakorns@gmail.com>
2016-05-02 01:55:58 +0700

    fixed some bug

--- models/order.js
+++ models/order.js
@@ -31,7 +31,7 @@ function Order() {
 
     this.getOrdersInShopFromStatus = function(shopId,orderStatus,callback){
     	connection.acquire(function(err, con) {
-            con.query('SELECT ob.order_id , c.cus_name , DATE_FORMAT(ob.order_date,\'%m-%d-%Y\') as order_date , op.grand_price , ob.order_status FROM order_bill ob INNER JOIN (SELECT customer.cus_id ,customer.shop_id, customer.cus_name FROM customer WHERE customer.shop_id = ?) c ON ob.cus_id = c.cus_id INNER JOIN (SELECT oi.order_id , SUM(oi.amount*i.item_price) as grand_price FROM ordered_item oi INNER JOIN item i ON oi.item_id = i.item_id GROUP BY oi.order_id) op ON ob.order_id = op.order_id WHERE ob.order_status = ? ORDER BY ob.order_id DESC ',[shopId,orderStatus], function(err, result) {
+            con.query('SELECT ob.order_id , c.cus_name , ob.order_date , op.grand_price , ob.order_status FROM order_bill ob INNER JOIN (SELECT customer.cus_id ,customer.shop_id, customer.cus_name FROM customer WHERE customer.shop_id = ?) c ON ob.cus_id = c.cus_id INNER JOIN (SELECT oi.order_id , SUM(oi.amount*i.item_price) as grand_price FROM ordered_item oi INNER JOIN item i ON oi.item_id = i.item_id GROUP BY oi.order_id) op ON ob.order_id = op.order_id WHERE ob.order_status = ? ORDER BY ob.order_id DESC ',[shopId,orderStatus], function(err, result) {
                 con.release();
                 callback(result);
             });
@@ -50,7 +50,16 @@ function Order() {
 
     this.getSearchOrdersInShop = function(shopId,searchWord,callback){
         connection.acquire(function(err, con) {
-            con.query('SELECT ob.order_id , c.cus_name , DATE_FORMAT(ob.order_date,\'%m-%d-%Y\') as order_date , op.grand_price , ob.order_status FROM order_bill ob INNER JOIN (SELECT customer.cus_id ,customer.shop_id, customer.cus_name FROM customer WHERE customer.cus_name like \'%'+searchWord+'%\') c ON ob.cus_id = c.cus_id INNER JOIN (SELECT oi.order_id , SUM(oi.amount*i.item_price) as grand_price FROM ordered_item oi INNER JOIN item i ON oi.item_id = i.item_id GROUP BY oi.order_id) op ON ob.order_id = op.order_id ORDER BY ob.order_id DESC ',[cusId], function(err, result) {
+            con.query('SELECT ob.order_id , c.cus_name , ob.order_date , op.grand_price , ob.order_status FROM order_bill ob INNER JOIN (SELECT customer.cus_id ,customer.shop_id, customer.cus_name FROM customer WHERE customer.cus_name like \'%'+searchWord+'%\') c ON ob.cus_id = c.cus_id INNER JOIN (SELECT oi.order_id , SUM(oi.amount*i.item_price) as grand_price FROM ordered_item oi INNER JOIN item i ON oi.item_id = i.item_id GROUP BY oi.order_id) op ON ob.order_id = op.order_id ORDER BY ob.order_id DESC ',[cusId], function(err, result) {
+                con.release();
+                callback(result);
+            });
+        });
+    }
+
+    this.addOrderInShop = function(shopId,cusId,callback){
+        connection.acquire(function(err, con) {
+            con.query('SELECT ob.order_id , c.cus_name , ob.order_date, op.grand_price , ob.order_status FROM order_bill ob INNER JOIN (SELECT customer.cus_id ,customer.shop_id, customer.cus_name FROM customer WHERE customer.cus_name like \'%'+searchWord+'%\') c ON ob.cus_id = c.cus_id INNER JOIN (SELECT oi.order_id , SUM(oi.amount*i.item_price) as grand_price FROM ordered_item oi INNER JOIN item i ON oi.item_id = i.item_id GROUP BY oi.order_id) op ON ob.order_id = op.order_id ORDER BY ob.order_id DESC ',[cusId], function(err, result) {
                 con.release();
                 callback(result);
             });
